7abd4433d9117027a3843044df7b1b03a4946de3
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-08-11 18:40:47 -0500

    ✨Programmatic Routing
    Event Routing with dynamic parameter.

--- components/LanguageCard.vue
+++ components/LanguageCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <figure class="bg-gray-100 rounded-xl p-8 m-8 sm:m-4 md:m-8">
+  <figure class="bg-gray-100 rounded-xl p-8 m-8 sm:m-4 md:m-8" @click="alertLang(route)">
     <img class="w-32 h-32 rounded-full mx-auto" :src="imageUrl" alt="" width="384" height="512">
     <div class="pt-6 space-y-4">
       <blockquote>
@@ -21,6 +21,17 @@ export default {
     name: {
       type: [String],
       required: true
+    },
+    route: {
+      type: [String],
+      required: true
+    }
+  },
+  methods: {
+    alertLang (route) {
+      this.$router.push({
+        path: `${route}`
+      })
     }
   }
 }
