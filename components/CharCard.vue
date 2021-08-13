8be2610b365f874c8b26b7ecb6075858b8fc1832
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-08-12 19:20:15 -0500

    ✨Create CharCard
    Create CharCard layout and copy to clipboard logic.

---
+++ components/CharCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <figure class="bg-gray-100 rounded-xl p-8 m-8 sm:m-4 md:m-8" @click="copyChar(char)">
+    <div class="pt-6 space-y-4">
+      <blockquote class="rounded-full">
+        <h1 class="text-5xl font-semibold text-center">
+          {{ char }}
+        </h1>
+        <input id="testing-code" type="hidden" :value="char">
+      </blockquote>
+    </div>
+  </figure>
+</template>
+
+<script>
+export default {
+  props: {
+    char: {
+      type: [String],
+      required: true
+    }
+  },
+  methods: {
+    copyChar (char) {
+      const elem = document.createElement('textarea')
+      elem.value = char
+      document.body.appendChild(elem)
+      elem.select()
+      document.execCommand('copy')
+      document.body.removeChild(elem)
+    }
+  }
+}
+</script>
