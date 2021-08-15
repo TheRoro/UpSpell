6ce26d54e3e06920cc2de7f4e23a487b3595d97b
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-08-15 14:04:25 -0500

    🎨 CharCard layout
    Add letter and name to CharCards. Add vertical padding to CharCard.

--- components/Home.vue
+++ components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-screen">
-    <Banner title="UpSpell" sub-title="Special Characters for all languages!" />
-    <div class="bg-dark grid xs:grid-cols-1 sm:grid-cols-2 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-10 py-14">
+    <Banner title="UpSpell" sub-title="Language Special Characters" />
+    <div class="grid xs:grid-cols-1 sm:grid-cols-2 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-10 py-14">
       <div v-for="lang in languages" :key="lang.name">
         <LanguageCard class="langCard" :name="lang.name" :image-url="lang.imageUrl" :route="lang.route" />
       </div>
@@ -15,25 +15,9 @@ export default {
     return {
       languages: [
         { name: 'Français', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Flag_of_France.png/1200px-Flag_of_France.png', route: 'fr' },
-        { name: 'Español', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Bandera_de_Espa%C3%B1a.svg/1200px-Bandera_de_Espa%C3%B1a.png', route: 'es' },
-        { name: 'Português', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_Portugal.svg', route: 'pt' },
-        { name: 'Deutsch', imageUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1000px-Flag_of_Germany.svg.png', route: 'de' },
-        { name: 'Français', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Flag_of_France.png/1200px-Flag_of_France.png', route: 'fr' },
-        { name: 'Español', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Bandera_de_Espa%C3%B1a.svg/1200px-Bandera_de_Espa%C3%B1a.png', route: 'es' },
-        { name: 'Português', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_Portugal.svg', route: 'pt' },
-        { name: 'Deutsch', imageUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1000px-Flag_of_Germany.svg.png', route: 'de' },
-        { name: 'Français', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Flag_of_France.png/1200px-Flag_of_France.png', route: 'fr' },
-        { name: 'Español', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Bandera_de_Espa%C3%B1a.svg/1200px-Bandera_de_Espa%C3%B1a.png', route: 'es' },
-        { name: 'Português', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_Portugal.svg', route: 'pt' },
-        { name: 'Deutsch', imageUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1000px-Flag_of_Germany.svg.png', route: 'de' },
-        { name: 'Français', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Flag_of_France.png/1200px-Flag_of_France.png', route: 'fr' },
-        { name: 'Español', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Bandera_de_Espa%C3%B1a.svg/1200px-Bandera_de_Espa%C3%B1a.png', route: 'es' },
-        { name: 'Português', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_Portugal.svg', route: 'pt' },
-        { name: 'Deutsch', imageUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1000px-Flag_of_Germany.svg.png', route: 'de' },
-        { name: 'Français', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Flag_of_France.png/1200px-Flag_of_France.png', route: 'fr' },
-        { name: 'Español', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Bandera_de_Espa%C3%B1a.svg/1200px-Bandera_de_Espa%C3%B1a.png', route: 'es' },
-        { name: 'Português', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_Portugal.svg', route: 'pt' },
-        { name: 'Deutsch', imageUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1000px-Flag_of_Germany.svg.png', route: 'de' }
+        { name: 'Español', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Bandera_de_Espa%C3%B1a.svg/1200px-Bandera_de_Espa%C3%B1a.png', route: 'es' }
+        // { name: 'Português', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_Portugal.svg', route: 'pt' },
+        // { name: 'Deutsch', imageUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1000px-Flag_of_Germany.svg.png', route: 'de' }
       ]
     }
   }
--- pages/fr/index.vue
+++ pages/fr/index.vue
@@ -1,12 +1,53 @@
 <template>
-  <LanguageDetails :chars="chars" original-name="Français" name="french" />
+  <LanguageDetails :chars="chars" original-name="Français" name="French" />
 </template>
 
 <script>
 export default {
   data () {
     return {
-      chars: ['é', 'à', 'è', 'ù', 'â', 'ê', 'î', 'ô', 'û', 'ç']
+      chars: [
+        {
+          letter: 'é',
+          name: 'E accent aigu'
+        },
+        {
+          letter: 'à',
+          name: 'A accent grave'
+        },
+        {
+          letter: 'è',
+          name: 'E accent grave'
+        },
+        {
+          letter: 'ù',
+          name: 'U accent grave'
+        },
+        {
+          letter: 'â',
+          name: 'A accent circonflexe'
+        },
+        {
+          letter: 'ê',
+          name: 'E accent circonflexe'
+        },
+        {
+          letter: 'î',
+          name: 'I accent circonflexe'
+        },
+        {
+          letter: 'ô',
+          name: 'O accent circonflexe'
+        },
+        {
+          letter: 'û',
+          name: 'U accent circonflexe'
+        },
+        {
+          letter: 'ç',
+          name: 'C cédille'
+        }
+      ]
     }
   }
 }
