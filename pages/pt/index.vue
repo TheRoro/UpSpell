097eb4338b7c5c299d6efefb866348a382dfd921
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-08-16 10:03:28 -0500

    ✨Add Portuguese

---
+++ pages/pt/index.vue
@@ -0,0 +1,78 @@
+<template>
+  <LanguageDetails :chars="chars" original-name="Português" name="Portuguese" />
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      chars: [
+        {
+          letter: 'á',
+          name: 'A acento agudo'
+        },
+        {
+          letter: 'é',
+          name: 'E acento agudo'
+        },
+        {
+          letter: 'í',
+          name: 'I acento agudo'
+        },
+        {
+          letter: 'ó',
+          name: 'O acento agudo'
+        },
+        {
+          letter: 'ú',
+          name: 'U acento agudo'
+        },
+        {
+          letter: 'à',
+          name: 'A acento grave'
+        },
+        {
+          letter: 'è',
+          name: 'E acento grave'
+        },
+        {
+          letter: 'ì',
+          name: 'I acento grave'
+        },
+        {
+          letter: 'ò',
+          name: 'O acento grave'
+        },
+        {
+          letter: 'ù',
+          name: 'U acento grave'
+        },
+        {
+          letter: 'ã',
+          name: 'A til'
+        },
+        {
+          letter: 'õ',
+          name: 'O til'
+        },
+        {
+          letter: 'â',
+          name: 'A acento circunflexo'
+        },
+        {
+          letter: 'ê',
+          name: 'E acento circunflexo'
+        },
+        {
+          letter: 'ô',
+          name: 'O acento circunflexo'
+        },
+        {
+          letter: 'ç',
+          name: 'Cedilha'
+        }
+      ]
+    }
+  }
+}
+</script>
