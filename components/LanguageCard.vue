7892fbe600701282fe3f36a12ac0783e18879aa0
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-08-15 20:46:49 -0500

    📈 Improved SEO
    Add Meta tags to improve SEO.

--- components/LanguageCard.vue
+++ components/LanguageCard.vue
@@ -1,6 +1,6 @@
 <template>
   <figure class="max-w-full w-72 bg-gray-100 rounded-xl m-auto p-8 mt-14 px-2" @click="alertLang(route)">
-    <img class="w-32 h-32 rounded-full mx-auto" :src="imageUrl" alt="" width="384" height="512">
+    <img class="w-32 h-32 rounded-full mx-auto" :src="imageUrl" :alt="name" width="384" height="512">
     <div class="pt-6 space-y-4">
       <blockquote>
         <p class="text-lg font-semibold text-center">
