a9009c63e1e7bad832a05e09e6a44105c514f4ed
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-08-17 17:05:45 -0500

    ✨ Add Copied Notification
    Add Notification with boolean, animation and event.

--- components/LanguageDetails.vue
+++ components/LanguageDetails.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="h-screen">
+    <div v-if="copied" class="fixed p-3 bg-yellow-300 m-2 rounded copied-card font-medium">
+      Copied to clipboard
+    </div>
     <Banner :title="originalName" :sub-title="`Special Characters for ${name.toLowerCase()}`" />
     <div class="grid xs:grid-cols-1 sm:grid-cols-2 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-10 py-14">
-      <div v-for="char in chars" :key="char">
-        <CharCard class="charCard" :char="char" />
+      <div v-for="char in chars" :key="char.name">
+        <CharCard class="charCard" :char="char" :copied="copied" @myEvent="copyClick" />
       </div>
     </div>
   </div>
@@ -25,6 +28,11 @@ export default {
       required: true
     }
   },
+  data () {
+    return {
+      copied: false
+    }
+  },
   head () {
     const name = this.name
     const originalName = this.originalName
@@ -33,6 +41,11 @@ export default {
       meta: [
         { hid: 'description', name: 'description', content: `${name} | ${originalName} UpSpell - Special Characters` }
       ]
+    }
+  },
+  methods: {
+    copyClick () {
+      this.copied = !this.copied
     }
   }
 }
@@ -49,4 +62,24 @@ export default {
     cursor: pointer;
   }
 
+  .copied-card {
+    animation: fade 1.5s ease-in-out;
+    animation-fill-mode: forwards;
+    box-shadow: 0 3px #F59E0B;
+  }
+
+@keyframes fade {
+    0% {
+        opacity: 0;
+    }
+    30% {
+        opacity: 1;
+    }
+    70% {
+        opacity: 1;
+    }
+    100% {
+        opacity: 0;
+    }
+}
 </style>
