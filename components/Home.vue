760bdb125c2add7d76ac421c1100d7ec7404546c
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-09-11 18:37:07 -0500

    🌐 Add Romanian

--- components/Home.vue
+++ components/Home.vue
@@ -17,9 +17,10 @@ export default {
         { name: 'Français', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Flag_of_France.png/1200px-Flag_of_France.png', route: 'fr' },
         { name: 'Español', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Bandera_de_Espa%C3%B1a.svg/1200px-Bandera_de_Espa%C3%B1a.png', route: 'es' },
         { name: 'Português', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_Portugal.svg/1280px-Flag_of_Portugal.svg.png', route: 'pt' },
-        { name: 'Italiano', imageUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/03/Flag_of_Italy.svg/1500px-Flag_of_Italy.svg.png', route: 'it' }
-        // { name: 'Română', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Flag_of_Romania.svg/1280px-Flag_of_Romania.svg.png', route: 'ro' }
-        // { name: 'Deutsch', imageUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1000px-Flag_of_Germany.svg.png', route: 'de' }
+        { name: 'Italiano', imageUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/03/Flag_of_Italy.svg/1500px-Flag_of_Italy.svg.png', route: 'it' },
+        { name: 'Română', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Flag_of_Romania.svg/1280px-Flag_of_Romania.svg.png', route: 'ro' }
+        // { name: 'Deutsch', imageUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1000px-Flag_of_Germany.svg.png', route: 'de' },
+        // { name: 'Русский язык', imageUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/f/f3/Flag_of_Russia.svg/1280px-Flag_of_Russia.svg.png', route: 'ru' },
       ]
     }
   }
---
+++ pages/ro/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <LanguageDetails :chars="chars" original-name="Română" name="Romanian" :image-url="imageUrl" />
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      chars: [
+        {
+          letter: 'ă',
+          name: 'A căciula'
+        },
+        {
+          letter: 'â',
+          name: 'A circumflexul'
+        },
+        {
+          letter: 'î',
+          name: 'I circumflexul'
+        },
+        {
+          letter: 'ș',
+          name: 'S virgulița'
+        },
+        {
+          letter: 'ț',
+          name: 'T virgulița'
+        }
+      ],
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Flag_of_Romania.svg/1280px-Flag_of_Romania.svg.png'
+    }
+  }
+}
+</script>
