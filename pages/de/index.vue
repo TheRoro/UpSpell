6529a17c9ff40dbfcb506664f1eb45a7915ce666
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-09-22 14:59:26 -0500

    🌐 Add German

---
+++ pages/de/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <LanguageDetails :chars="chars" original-name="Deutsch" name="German" :image-url="imageUrl" />
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      chars: [
+        {
+          letter: 'ä',
+          name: 'A umlaut'
+        },
+        {
+          letter: 'ö',
+          name: 'O umlaut'
+        },
+        {
+          letter: 'ü',
+          name: 'U umlaut'
+        },
+        {
+          letter: 'ß',
+          name: 'Eszett/Scharfes S'
+        }
+      ],
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1000px-Flag_of_Germany.svg.png'
+    }
+  }
+}
+</script>
